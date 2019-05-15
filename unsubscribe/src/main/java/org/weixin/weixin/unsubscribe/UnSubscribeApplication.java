8145b28a76dc907e0bdf9d94cd531158c4647d32
ASUS <ASUS@DESKTOP-KVT4ML0>
2019-05-15 10:24:49 +0800

    修改日志

--- unsubscribe/src/main/java/org/weixin/weixin/unsubscribe/UnSubscribeApplication.java
+++ unsubscribe/src/main/java/org/weixin/weixin/unsubscribe/UnSubscribeApplication.java
@@ -55,7 +55,7 @@ public class UnSubscribeApplication implements EventListenerConfig  , Applicatio
 			}
 		} catch (Exception e) {
 			LOG.warn("Bean的ID {} 无法调用对应的消息处理器: {}" , id , e.getMessage());
-			LOG.trace(e.getMessage(), e);
+//			LOG.trace(e.getMessage(), e);
 		}
 	}
 	
